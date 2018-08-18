3c9586c13bd839180c29c072e19d39c1993b8da4
lei2350934xj <510539175@qq.com>
2018-08-19 00:47:00 +0800

    基本流程-6part

---
+++ myApp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Grades(models.Model):
+    # 继承自 models.Model
+    gname = models.CharField(max_length=20)
+    gdate = models.DateTimeField()
+    ggirlnum = models.IntegerField()
+    gboynum = models.IntegerField()
+    isDelete = models.BooleanField(default=False)
+
+
+class Students(models.Model):
+    sname = models.CharField(max_length=20)
+    sgender = models.BooleanField(default=True)
+    sage = models.IntegerField()
+    scontend = models.CharField(max_length=20)
+    isDelete = models.BooleanField(default=False)
+    sgrade = models.ForeignKey("Grades",on_delete=models.CASCADE)    # 关联外键
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myApp'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "myDB",
+        'USER': 'root',
+        'PASSWORD': 'liangting01',
+        'HOSTNAME': 'localhost',
+        'PORT': '3306'
     }
 }
 
